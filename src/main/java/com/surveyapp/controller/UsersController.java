adee6756faf7c710f52bc92be5cdd00fe4b83ff1
hakankhrmn <hakan-kahraman1099@hotmail.com>
2021-08-25 17:03:16 +0300

    Adding createUser for ADMIN_USER

--- src/main/java/com/surveyapp/controller/UsersController.java
+++ src/main/java/com/surveyapp/controller/UsersController.java
@@ -1,4 +1,27 @@
 package com.surveyapp.controller;
 
+import com.surveyapp.model.dto.UserDto;
+import com.surveyapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsersController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create-user")
+    @PreAuthorize("hasAnyAuthority('ADMIN_USER')")
+    public ResponseEntity<?> createUser(@RequestBody UserDto userDto) throws Exception {
+        return ResponseEntity.ok(userService.save(userDto));
+    }
 }
